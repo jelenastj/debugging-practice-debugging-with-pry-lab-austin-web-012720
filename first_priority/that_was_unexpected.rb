03e0f667b2a0f0b3cb0dd98f4874dce410596238
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-18 01:11:59 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,9 +1,12 @@
+
 class Person
+  
   attr_accessor :name, :candy_lover
 
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
+    
   end
 
   def tall?
@@ -24,6 +27,7 @@ class Person
     else
       10.times do
         "not a ballin candy lover"
+      end    
     end
   end
 end
